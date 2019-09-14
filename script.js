d1d6c44f2eb5df5ab04470b0be8e892d9738c8c7
Anni <anni@winnersjo.se>
2019-09-14 16:18:09 +0200

    test

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-{
-    const submit = function () {
-        alert('I am here');
-    };
 
+    const CHOICES = ['rock','paper','scissors']
+    let computerChoice = CHOICES[Math.floor(Math.random() *CHOICES.length)]
+    console.log(computerChoice)
+    let userChoice
+
+    const submit = function () {
+ userChoice = document.querySelector('.radio:checked').value    
+}
+
+const checkWinner = () =>{
+let isUserWinner
+if(userChoice === 'rock' && computerChoice === 'scissors') {
+    isUserWinner = true
+} else if (userChoice === 'paper' && computerChoice === 'rock') {
+    isUserWinner = true
+} else if (userChoice === 'scissors' && computerChoice === 'paper') {
+    isUserWinner = true
+} else if (userChoice === computerChoice) {
+    is isUserWinner = false
+} else {
+    isUserWinner = false
+}
+return isUserWinner
 }
